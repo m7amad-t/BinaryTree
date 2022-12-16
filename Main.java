0587a90fe7c8efedaf55c986c5b5917a279f3524
Mohammad <99071291+m7amad-t@users.noreply.github.com>
2022-12-17 00:48:33 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree ob = new BinaryTree();
+        ob.add(20);
+        ob.add(25);
+        ob.add(35);
+        ob.add(28);
+        ob.add(21);
+        ob.add(22);
+        ob.add(23);
+        ob.add(19);
+        ob.add(10);
+        ob.add(9);
+        ob.add(8);
+        ob.add(7);
+        ob.add(6);
+        ob.add(5);
+        ob.add(4);
+
+        ob.traverse();
+
+
+
+
+
+    }
+}
